9e6271c8a60eb61927fcbbdaad5e8b942b8c403c
Stan Mayer <sthumor@gmail.com>
2024-12-07 12:13:47 +0200

    CW47

--- src/main/java/telran/net/TcpServer.java
+++ src/main/java/telran/net/TcpServer.java
@@ -10,16 +10,21 @@ public class TcpServer implements Runnable{
     }
         @Override
         public void run() {
+            //FIXME add SocketTimeOut handling for shutdown 
            try (ServerSocket serverSocket = new ServerSocket(port)) {
              System.out.println("Server is listening on the port "+ port);
                 while(true) {
                     Socket socket = serverSocket.accept();
                     var session = new TcpClientServerSession(protocol, socket);
-                    session.run();
+                    Thread thread = new Thread(session);
+                    thread.start();;
                 }
            } catch (Exception e) {
             System.out.println(e);
            }
         }
-    
+        public void shutdown() {
+            //TODO
+            //In the ExecutorService framework to provide shutdownNow (to ignore all not processing client sessions)
+        }
     }
