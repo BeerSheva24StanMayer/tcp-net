e725d0f1d8ef30353f6eeacb3048bbc958497aaa
Stan Mayer <sthumor@gmail.com>
2024-12-07 12:16:06 +0200

    CW47

--- src/main/java/telran/net/TcpServer.java
+++ src/main/java/telran/net/TcpServer.java
@@ -1,30 +1,31 @@
 package telran.net;
 import java.net.*;
-
 public class TcpServer implements Runnable{
-    Protocol protocol;
-    int port;
-    public TcpServer(Protocol protocol, int port) {
-        this.protocol = protocol;
-        this.port = port;
+Protocol protocol;
+int port;
+public TcpServer(Protocol protocol, int port) {
+    this.protocol = protocol;
+    this.port = port;
+}
+    @Override
+    public void run() {
+        //FIXME add SocketTimeOut handling for shutdown 
+       try (ServerSocket serverSocket = new ServerSocket(port)) {
+         System.out.println("Server is listening on the port "+ port);
+            while(true) {
+                Socket socket = serverSocket.accept();
+                var session = new TcpClientServerSession(protocol, socket);
+                Thread thread = new Thread(session);
+
+                thread.start();
+            }
+       } catch (Exception e) {
+        System.out.println(e);
+       }
     }
-        @Override
-        public void run() {
-            //FIXME add SocketTimeOut handling for shutdown 
-           try (ServerSocket serverSocket = new ServerSocket(port)) {
-             System.out.println("Server is listening on the port "+ port);
-                while(true) {
-                    Socket socket = serverSocket.accept();
-                    var session = new TcpClientServerSession(protocol, socket);
-                    Thread thread = new Thread(session);
-                    thread.start();;
-                }
-           } catch (Exception e) {
-            System.out.println(e);
-           }
-        }
-        public void shutdown() {
-            //TODO
-            //In the ExecutorService framework to provide shutdownNow (to ignore all not processing client sessions)
-        }
+    public void shutdown() {
+        //TODO
+        //In the ExecutorService framework to provide shutdownNow (to ignore all not processing client sessions)
     }
+
+}
